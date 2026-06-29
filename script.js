253d1c4278a92c0bf60328c985ee2e8f20cbea9c
Deepak <darkdeepak001@gmail.com>
2026-06-29 21:53:43 +0530

    Add submitRecommendation function for alerts

--- script.js
+++ script.js
@@ -8,3 +8,7 @@ function closenavbar()
 {
     sidenav.style.left="-60%"
 }
+function submitRecommendation()
+{
+    alert("🎉 Thank you for recommending a book!\n\nOur team will review your recommendation.\nHappy Reading 📚❤️");
+}
